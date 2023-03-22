53554b895224dd4e5c423484b9bbae950d0e0976
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-03-22 05:28:02 -0600

    adding cardpopup call

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import logo from './img/pokeball.png';
 import { handleScroll, handleScrollTop } from './modules/scroll.js';
 // import cardpopup from './modules/reservationspopup.js';
 import { fetchLikes, postLike } from './modules/likes.js';
+import cardpopup from './modules/reservationspopup'
 
 const img = document.createElement('img');
 img.src = logo;
