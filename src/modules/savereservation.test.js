aa69b0be16a1b2bf7156ca30ecf7df10342020ab
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-03-23 14:36:51 -0600

    fixing linter errors

--- src/modules/savereservation.test.js
+++ src/modules/savereservation.test.js
@@ -1,20 +1,20 @@
 const table = [
-    {
-     item1: 1,
-    },
-    {
-      item1: 2,
-    },
-    {
-     item1: 3,
-    }
-]
+  {
+    item1: 1,
+  },
+  {
+    item1: 2,
+  },
+  {
+    item1: 3,
+  },
+];
 
 const calculatereservations = () => {
-    const counter = table.length;
-    return counter;
-  };
+  const counter = table.length;
+  return counter;
+};
 
-test ('Calculator should return array length', () => {
-    expect(calculatereservations()).toBe(3);
-})
+test ( 'Calculator should return array length', () => {
+  expect(calculatereservations()).toBe(3);
+});
