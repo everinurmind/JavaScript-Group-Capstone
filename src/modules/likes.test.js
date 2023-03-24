472055bfb1cba656e3c4abe518155847a9f97339
everinurmind <nurbol.sultanov@gmail.com>
2023-03-23 19:59:58 -0600

    Fix test file

--- src/modules/likes.test.js
+++ src/modules/likes.test.js
@@ -1,23 +1,58 @@
 import { fetchLikes, postLike, calculateLikes } from './likes.js';
 
 describe('fetchLikes', () => {
-  it('should return an array with the number of likes', async () => {
+  it('should return the number of likes', async () => {
+    global.fetch = jest.fn().mockResolvedValueOnce({
+      json: () => Promise.resolve([{ item_id: 123, likes: 42 }]),
+    });
+
     const result = await fetchLikes(123);
-    expect(Array.isArray(result)).toBe(true);
-    expect(typeof result[0]).toBe('number');
+
+    expect(result).toBe(42);
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(
+      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/LwQHc3Vs1lC9z58aI8eY/likes',
+    );
+
+    global.fetch.mockRestore();
   });
 
-  it('should return an empty array if there is an error', async () => {
-    const result = await fetchLikes(-1);
-    expect(Array.isArray(result)).toBe(true);
-    expect(result.length).toBe(1);
-    expect(result[0]).toBe(0);
+  it('should return 0 if there is an error', async () => {
+    global.fetch = jest.fn().mockRejectedValueOnce({ message: 'Fetch error' });
+
+    try {
+      const result = await fetchLikes(123);
+      expect(result).toBe(0);
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(
+      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/LwQHc3Vs1lC9z58aI8eY/likes',
+    );
+
+    global.fetch.mockRestore();
   });
 });
 
 describe('postLike', () => {
   it('should not throw an error', async () => {
-    await expect(postLike(123)).resolves.not.toThrow();
+    global.fetch = jest.fn().mockResolvedValueOnce();
+
+    await expect(postLike(123, 42)).resolves.not.toThrow();
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(
+      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/LwQHc3Vs1lC9z58aI8eY/likes',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ item_id: 123, likes: 42 }),
+      },
+    );
+
+    global.fetch.mockRestore();
   });
 });
 
@@ -36,14 +71,18 @@ describe('calculateLikes', () => {
       </div>
     `;
 
-    // Mock the fetchLikes function
-    global.fetch = jest.fn().mockResolvedValue({ likes: [0] });
+    global.fetch = jest.fn()
+      .mockResolvedValueOnce({ json: () => Promise.resolve([{ item_id: 'item-1', likes: 42 }, { item_id: 'item-2', likes: 13 }]) })
+      .mockResolvedValueOnce({ json: () => Promise.resolve([{ item_id: 'item-1', likes: 44 }, { item_id: 'item-2', likes: 15 }]) });
 
     await calculateLikes();
 
     expect(global.fetch).toHaveBeenCalledTimes(2);
-    expect(document.querySelector('#item-1 .like-num').textContent).toBe('0');
-    expect(document.querySelector('#item-2 .like-num').textContent).toBe('0');
+    expect(global.fetch).toHaveBeenCalledWith(
+      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/LwQHc3Vs1lC9z58aI8eY/likes',
+    );
+    expect(document.querySelector('#item-1 .like-num').textContent).toBe('42');
+    expect(document.querySelector('#item-2 .like-num').textContent).toBe('15');
 
     global.fetch.mockRestore();
   });
